33091a5609cc07b673a5b4feb0141b6db226259c
Ioanna Kartsonaki <yynakar.io@gmail.com>
2022-11-13 21:30:58 +0200

    Upvoting with put req

--- src/server.js
+++ src/server.js
@@ -1,16 +1,30 @@
 import express from 'express';
 
+let articlesInfo=[{
+    name: 'learn-react',
+    upvotes: 0,
+},
+{
+    name:'learn-node',
+    upvotes:0,
+},
+{
+    name:'mongodb',
+    upvotes:0,
+}]
+
 const app=express();
 app.use(express.json());
 
-app.post('/hello',(req, res)=>{
-    res.send(`Hello ${req.body.name}`);
-});
-
-app.get('/hello/:name/gn/:otherName',(req,res)=>{
-    console.log(req.params);
-    const {otherName}=req.params;
-    res.send(`Hello ${otherName}!!`);
+app.put('/api/articles/:name/upvote',(req,res)=>{
+    const {name}=req.params;
+    const article=articlesInfo.find(a=>a.name===name);
+    if(article){
+        article.upvotes+=1;
+        res.send(`The ${name} article has ${article.upvotes} upvotes.`);
+    } else{
+        res.send(`The ${name} article doesn\'t exist.`);
+    }
 })
 
 app.listen(8000,()=>{
