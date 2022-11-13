c7db91197161f15dd603aaee07c5597dcb41ee25
Ioanna Kartsonaki <yynakar.io@gmail.com>
2022-11-13 21:11:21 +0200

    Practice requests with express and postman

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 import express from 'express';
 
 const app=express();
+app.use(express.json());
 
-app.get('/hello',(req, res)=>{
-    res.send('Hello server world!');
+app.post('/hello',(req, res)=>{
+    res.send(`Hello ${req.body.name}`);
 });
+
+app.get('/hello/:name/gn/:otherName',(req,res)=>{
+    console.log(req.params);
+    const {otherName}=req.params;
+    res.send(`Hello ${otherName}!!`);
+})
 
 app.listen(8000,()=>{
     console.log('Server is listening on port 8000');
 });
+
