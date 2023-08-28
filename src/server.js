d6deaba3f9151f82ffb3b7c44866bf854804cff6
Ioanna Kartsonaki <yynakar.io@gmail.com>
2023-08-29 00:26:28 +0300

    update upvotes in put

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.get('/api/articles/:name', async (req, res) => {
     const { name } = req.params;
 
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    const client = new MongoClient('mongodb://localhost:27017');
     await client.connect();
 
     const db = client.db('react-blog-db');
@@ -22,11 +22,23 @@ app.get('/api/articles/:name', async (req, res) => {
 
 })
 
-app.put('/api/articles/:name/upvote', (req, res) => {
+app.put('/api/articles/:name/upvote', async (req, res) => {
     const { name } = req.params;
-    const article = articlesInfo.find(a => a.name === name);
+  //  const article = articlesInfo.find(a => a.name === name);//fake local db
+
+    const client= new MongoClient('mongodb://localhost:27017');
+    await client.connect();
+
+    const db = client.db('react-blog-db');
+
+    db.collection('articles').updateOne({name},{
+        $inc:{ upvotes:1}, 
+    })
+
+    const article=await db.collection('articles').findOne({name});
+
     if (article) {
-        article.upvotes += 1;
+       // article.upvotes += 1;
         res.send(`The ${name} article has ${article.upvotes} upvotes!`);
     } else {
         res.send(`The ${name} article doesn\'t exist.`);
@@ -45,7 +57,7 @@ app.post('/api/articles/:name/comments', (req, res) => {
     }
 })
 
-app.listen(8000, () => {
-    console.log('Server is listening on port 8000');
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
 });
 
